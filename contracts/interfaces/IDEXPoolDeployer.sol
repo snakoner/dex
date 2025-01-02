f2eb61d7eed00cfbc8c966adce55aa39b4f962cf
snakoner <andrstroganov@yandex.ru>
2025-01-02 18:31:41 +0300

    update contracts

--- contracts/interfaces/IDEXPoolDeployer.sol
+++ contracts/interfaces/IDEXPoolDeployer.sol
@@ -2,14 +2,12 @@
 pragma solidity ^0.8.28;
 
 interface IDEXPoolDeployer {
-        function parameters()
-        external
-        view
+        function parameters() 
+        external view
         returns (
             address factory,
             address token0,
             address token1,
             uint24 fee
         );
-
 }
